59fbe88cd4f712a6bba4d53f2770c3b2bda5c3df
Tyler Witten <turtlewit@gmail.com>
2018-10-17 11:56:23 -0700

    initial commit

---
+++ Assets/UnitySpriteScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitySpriteScript : MonoBehaviour {
+
+	public string horizontal_axis_name = "Horizontal";
+	public string vertical_axis_name = "Vertical";
+	public string rotate_button_name = "Jump";	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
